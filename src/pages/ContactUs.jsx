5c597400add9e2f447ae09423422e97c11c49bf9
Saurav-Shinde <sauravshindee26@gmail.com>
2026-02-26 14:42:31 +0530

    meeting cost update

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useCallback } from "react";
 import Layout from "../components/Layout";
 import api from "../utils/api";
 
-const MEETING_COST = 50;
+const MEETING_COST = 60;
 
 function storageAvailable() {
   try {
